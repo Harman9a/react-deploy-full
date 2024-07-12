5deec4187bffa206fb001e2fdfb70dbc142268e1
harman9a <harman9a@gmail.com>
2024-07-12 12:26:56 +0530

    backup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React from "react";
+import "./App.css";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./logo.svg" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
